48ebf9c434a87232fbdedba2629162461c5761da
Dmitry <fiskdmitry@gmail.com>
2025-01-27 23:37:34 +0300

    zh-CN and zh-TW alternative button

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import "./assets/styles.css";
 import CustomGameSettings from "./ui";
 import bots from "./bots";
 
+const zhButtonAlternative = [ "zh-CN", "zh-TW" ]; // I'm sorry if this incorrect
+
 class CustomGameButton {
     constructor() {
         this.observer = null;
@@ -24,27 +26,32 @@ class CustomGameButton {
         });
     }
 
-    tryAppendButton() {
+    async tryAppendButton() {
         const navMenu = document.querySelector(".left-nav-menu");
-        if (!navMenu || document.querySelector(".force-custom-game")) return;
+        if (!navMenu || document.querySelector(".fcg-root")) return;
 
-        this.button = this.createButtonElement();
-        navMenu.insertBefore(this.button, navMenu.firstChild);
+        this.button = await this.createButtonElement();
+        if (this.button) navMenu.insertBefore(this.button, navMenu.firstChild);
     }
 
-    createButtonElement() {
+    async createButtonElement() {
+        const locale = await getClientLocale();
+
         const rootDiv = document.createElement("div");
         const buttonDiv = document.createElement("div");
         const button = document.createElement("div");
 
-        rootDiv.className = "navigation-status-ticker has-incidents ember-view";
-        buttonDiv.className = "ticker-button force-custom-game";
+        rootDiv.className = "navigation-status-ticker has-incidents ember-view fcg-root";
+        buttonDiv.className = zhButtonAlternative.includes(locale) ? "ticker-button force-custom-game-zh" : "ticker-button force-custom-game";
         button.className = "ticker-toggle";
 
         buttonDiv.addEventListener("click", () => createLobby());
         
         buttonDiv.append(button);
         rootDiv.append(buttonDiv);
+
+        if (document.querySelector(".fcg-root")) return null; // this prevents button duplicating when league client is lagging af (for example when you reload it way too much times or smth else)
+
         return rootDiv;
     }
 }
@@ -89,6 +96,13 @@ async function createLobby() {
     } catch (error) {
         console.error(`Failed to create lobby: ${error}`);
     }
+}
+
+export async function getClientLocale() {
+    const localeRequest = await fetch(`/riotclient/region-locale`);
+    const localeData = await localeRequest.json();
+
+    if (localeData.locale) return localeData.locale;
 }
 
 export function load() {
